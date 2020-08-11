bd3dc481b89849df98c51a85aa2165fe95407289
Daesik Choi <seafood0917@gmail.com>
2020-08-11 21:46:50 +0900

    Doing year and population scraping.

--- worldometers/worldometers/spiders/countries.py
+++ worldometers/worldometers/spiders/countries.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import logging
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
@@ -19,4 +19,14 @@ class CountriesSpider(scrapy.Spider):
             #absolute_link = response.urljoin(link)
             #yield scrapy.Request(url=absolute_link)
 
-            yield response.follow(url=link)
+            yield response.follow(url=link, callback=self.parse_country)
+    def parse_country(self, response):
+        #logging.info(response.url)
+        rows = response.xpath("(//table[@class='table table-striped table-bordered table-hover table-condensed table-list'])[1]/tbody/tr")
+        for row in rows:
+            year = row.xpath(".//td[1]/text()").get()
+            population = row.xpath(".//td[2]/strong/text()").get()
+            yield{
+                'year' : year,
+                'population' : population,
+            }
